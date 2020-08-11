93e3586b22b33b555d6f981a64ef272f60d16851
michaelhu <hbj148@gmail.com>
2020-08-12 00:37:28 +0800

    update src

---
+++ src/Console/Commands/SaasPolymerInstallCommand.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace ArtisanCloud\SaaSMonomer\Console\Commands;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Console\Command;
+
+
+
+class SaasPolymerInstallCommand extends Command
+{
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'saasmonomer:install';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'saasmonomer:install';
+
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle(): int
+    {
+//        dd($this->arguments());
+//        dd($this->options());
+
+//        Artisan::call('passport:install');
+        $this->info('install saas polymer');
+
+        return 0;
+    }
+
+
+
+}
---
+++ src/Providers/PolymerServiceProvider.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace ArtisanCloud\SaaSPolymer\Providers;
+
+use Laravel\Passport\Passport;
+
+use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class PolymerServiceProvider
+ * @package ArtisanCloud\SaaSPolymer\Providers
+ */
+class PolymerServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+//        $this->app->register(
+//            TenantServiceProvider::class
+//        );
+    }
+
+    /**
+     * Bootstrap services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        // make sure passport is installed
+        Passport::routes(function ($router) {
+            // RouteRegistrar->forAccessTokens()
+            $router->forAccessTokens();
+        }, ['middleware' => 'checkHeader']);
+        Passport::tokensExpireIn(now()->addDays(90));
+        Passport::refreshTokensExpireIn(now()->addDays(90));
+
+
+
+        if ($this->app->runningInConsole()) {
+
+//            $this->commands([
+//                SaasPolymerInstallCommand::class,
+//            ]);
+
+//                $this->publishes([
+//                    __DIR__ . '/../'.SaaSPolymerCommand::FOLDER_MIGRATION.'/migrations' => "/../" . app_path(),
+//                ], 'saas-monomer-migrations');
+        }
+    }
+}
